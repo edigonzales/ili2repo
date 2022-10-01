7fe6acf231130c5f4c76c4ec2748b82900d5b5a9
edigonzales <edi.gonzales@gmail.com>
2022-10-01 20:29:47 +0200

    initial commit

---
+++ app/src/main/java/ch/so/agi/ili2repo/App.java
@@ -0,0 +1,41 @@
+package ch.so.agi.ili2repo;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Help;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Help.Ansi;
+
+@Command(
+        name = "ili2repo",
+        description = "Creates an ilimodel.xml file from a directory with INTERLIS models.",
+        //version = "ili2repo version 0.0.1",
+        mixinStandardHelpOptions = true,
+        
+        headerHeading = "%n",
+        //synopsisHeading = "%n",
+        descriptionHeading = "%nDescription: ",
+        parameterListHeading = "%nParameters:%n",
+        optionListHeading = "%nOptions:%n"
+      )
+public class App implements Callable<Integer> {
+
+    @Option(names = { "-d", "--directory" }, required = true, paramLabel = "MODELDIR", description = "The directory containing the INTERLIS models.")
+    File archive;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.out.println("Hallo Welt.");
+        return 0;
+    }
+    
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App())
+                .setColorScheme(Help.defaultColorScheme(Ansi.AUTO))
+                .execute(args);
+        System.exit(exitCode);
+    }
+}
---
+++ app/src/test/java/ch/so/agi/ili2repo/AppTest.java
@@ -0,0 +1,12 @@
+package ch.so.agi.ili2repo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+   // @Test 
+    public void appHasAGreeting() {
+        //App classUnderTest = new App();
+        //assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
